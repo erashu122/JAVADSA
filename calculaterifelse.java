86eb0eed54dafbad4ff51a9bd805c9ccaa556bcb
erashu122 <kbitu8676@gmail.com>
2025-07-20 19:17:29 +0530

    first commit

---
+++ calculaterifelse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class calculaterifelse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        char op=sc.next().charAt(0);
+        int b=sc.nextInt();
+
+        if (op =='+') System.out.println(a+b);
+        if (op =='-') System.out.println(a-b);
+        if (op =='*') System.out.println(a*b);
+        if (op =='/') System.out.println(a/b);
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,11 @@
+class mcq {
+    public static void main(String[] args) {
+        int x=3,y=5;
+        if (x == 3)
+            System.out.println(x);
+        else      //not print y ;
+            System.out.println(y); //output :3
+//        else; //is there printing the y becouse of (;) becouse; is break the line
+//        System.out.println(y); //output 3 5
+    }
+}
