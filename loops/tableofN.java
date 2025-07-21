cec8df97289d124edbedaa79bb259932885f4c2e
erashu122 <kbitu8676@gmail.com>
2025-07-21 10:18:27 +0530

    first commit

---
+++ loops/tableofN.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class tableofN {
+    public static void main(String[] args) {
+        int n=19;
+        for(int i=1;i<=10;i++){
+            System.out.println(i*n);
+        }
+    }
+
+}
