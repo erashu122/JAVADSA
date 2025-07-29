41e4321499f9d6f4b33fd61b98bbf5f32c19aeae
erashu122 <kbitu8676@gmail.com>
2025-07-29 18:11:47 +0530

    first commit

--- DAS_WITH_JAVAADD/Array/sortArray.java
+++ DAS_WITH_JAVAADD/Array/sortArray.java
@@ -4,18 +4,41 @@ public class sortArray {
     public static void main(String[] args) {
         int[] arr={0,1,0,0,0,1,1,1,0,0};
         int n=arr.length;
-        int noofZero=0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i]==0) noofZero++;
+ //       int noofZero=0;
+//        METHOD 1
+//        for (int i = 0; i < n; i++) {
+//            if (arr[i]==0) noofZero++;
+//        }
+//        for (int i = 0; i < n; i++) {
+//            if (i<noofZero) arr[i]=0;
+//            else arr[i]=1;
+//        }
+//        for (int ele :arr){
+//            System.out.print(ele+" ");
+//        }
+//        System.out.println();
 
 
+        //METHOD 2 --> TWO PAS SOLUTION
+
+        int i=0,j=n-1;
+        while(i<j){
+            if(arr[i]==0) i++;
+            if(arr[j]==1) j--;
+            if (i>j) break;
+            if(arr[i]==1 && arr[j]==0){
+                arr[i]=0;
+                arr[j]=1;
+                i++;
+                j--;
+            }
         }
-        for (int i = 0; i < n; i++) {
-            if (i<noofZero) arr[i]=0;
-            else arr[i]=1;
-
-
+        for(int ele:arr){
+            System.out.print(ele+" ");
         }
+        System.out.println( );
+
+
 
     }
 }
