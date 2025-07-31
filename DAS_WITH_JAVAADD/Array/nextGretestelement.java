bec2c09aa556a6b679287449ff01a3652ad61c80
erashu122 <kbitu8676@gmail.com>
2025-07-31 11:01:18 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/Array/nextGretestelement.java
@@ -0,0 +1,26 @@
+package DAS_WITH_JAVAADD.Array;
+
+public class nextGretestelement {
+    public static void main(String[] args) {
+         int[] arr={12,8,41,34,23,90,54,32,55,65};
+         int n=arr.length;
+         int[] ans=new int[n];
+         ans[n-1]=-1;
+        for (int i = 0; i < n-1; i++) {
+            int mx=Integer.MIN_VALUE;
+            for (int j = i+1; j < n; j++) {
+                mx=Math.max(mx,arr[j]);
+
+            }
+            ans[i]=mx;
+        }
+        for (int ele:arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        for (int ele:ans){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+}
