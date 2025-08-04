89451c1afe5160ab562ace155fcbc1a2725d1e9c
erashu122 <kbitu8676@gmail.com>
2025-08-04 20:30:06 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/Array/TwoDimensionalArray/foreach.java
@@ -0,0 +1,22 @@
+package DAS_WITH_JAVAADD.Array.TwoDimensionalArray;
+
+public class foreach {
+    public static void main(String[] args) {
+        int[] a={7,5,6,4};
+        for (int ele:a){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+
+        //1 2 3
+        //4 5 6
+        int[][] b={{1,2,3},{4,5,6}};
+        for(int[] ele:b ){
+            for (int x:ele){
+                System.out.print(x+" ");
+            }
+            System.out.println( );
+
+        }
+    }
+}
