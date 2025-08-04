255305676fd1c108ba65c6d1f541548beb45ba93
erashu122 <kbitu8676@gmail.com>
2025-08-04 21:57:09 +0530

    first commit

--- DAS_WITH_JAVAADD/Array/TwoDimensionalArray/Addition_of_TwoMatrix.java
+++ DAS_WITH_JAVAADD/Array/TwoDimensionalArray/Addition_of_TwoMatrix.java
@@ -8,17 +8,29 @@ public class Addition_of_TwoMatrix {
         int n=a.length;
         int m=a[0].length;
         int[][] res=new int[n][m];
+        System.out.println("with using new matrix :");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 res[i][j]=a[i][j]+b[i][j];
                 System.out.print(res[i][j]+" ");
-
+            }
+            System.out.println();
+        }
+        System.out.println("without using any new matrix :->");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j]+=b[i][j];
+                System.out.print(a[i][j]+" ");
             }
             System.out.println();
         }
     }
 }
-// output:
+//with using new matrix :
+//        13 15 17
+//        19 17 38
+//        41 53 43
+//without using any new matrix :->
 //        13 15 17
 //        19 17 38
 //        41 53 43
