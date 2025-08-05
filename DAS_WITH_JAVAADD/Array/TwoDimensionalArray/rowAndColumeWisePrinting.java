edd11427444479d23c2b6312e8c4784c6615573b
erashu122 <kbitu8676@gmail.com>
2025-08-05 09:26:45 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/Array/TwoDimensionalArray/rowAndColumeWisePrinting.java
@@ -0,0 +1,28 @@
+package DAS_WITH_JAVAADD.Array.TwoDimensionalArray;
+
+public class rowAndColumeWisePrinting {
+    public static void main(String[] args) {
+        int[][] a={{1,2,3},{5,7,5}};
+        int n=a.length;
+        int m=a[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[j][i]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        int[][] transpose=new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[i][j]=a[j][i];
+                System.out.print(transpose[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
