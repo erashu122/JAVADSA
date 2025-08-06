db481a2cf7153e517400606c04b3526a058664bf
erashu122 <kbitu8676@gmail.com>
2025-08-06 09:36:40 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/Array/TwoDimensionalArray/Webform.java
@@ -0,0 +1,102 @@
+package DAS_WITH_JAVAADD.Array.TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class Webform {
+//    public static int[][] taking() {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("Enter the Order of Matrix (m*n) :");
+//        int m= sc.nextInt();
+//        int n= sc.nextInt();
+//        int[][] ar = new int[m][n];
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print("Enter the element "+i+" "+j+" :");
+//                ar[i][j]=sc.nextInt();
+//            }
+//            System.out.println();
+//        }
+//        return ar;
+//    }
+    public static void print(int[][] arr) {
+        int m=arr.length,n=arr[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}} ;
+        int m=arr.length, n=arr[0].length;
+        System.out.println("Original matrix :");
+        print(arr);
+        System.out.println("WEBFORM matrix (S):");
+        for (int i = 0; i < m; i++) {
+            if (i%2==0){
+                for (int j = n-1; j >= 0; j--) {
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+
+            }
+            else {
+                for (int j =0; j <n; j++) {
+                    System.out.print(arr[i][j]+" ");
+
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("WEBFORM matrix form ->2:");
+        for (int i = 0; i < m; i++) {
+            if (i%2==0){
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+
+            }
+            else {
+                for (int j =n-1; j >=0; j--) {
+                    System.out.print(arr[i][j]+" ");
+
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("WEBFORM matrix form ->3:");
+        if (n%2==0){
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <m; j++) {
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+        }
+    }
+}
+/*Enter the Order of Matrix (m*n) :3 3
+Enter the element 0 0 :1
+Enter the element 0 1 :2
+Enter the element 0 2 :3
+
+Enter the element 1 0 :4
+Enter the element 1 1 :5
+Enter the element 1 2 :6
+
+Enter the element 2 0 :7
+Enter the element 2 1 :8
+Enter the element 2 2 :9
+
+Original matrix :
+1 2 3
+4 5 6
+7 8 9
+
+WEBFORM matrix :
+1 2 3
+6 5 4
+7 8 9
+*/
