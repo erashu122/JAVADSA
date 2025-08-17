92e0af097f25906a0fa5bb2fdf19ff66015c0c31
erashu122 <kbitu8676@gmail.com>
2025-08-17 11:33:26 +0530

    first commit

---
+++ JAVABASICS/loops/dowhileLoop.java
@@ -0,0 +1,12 @@
+package JAVABASICS.loops;
+
+public class dowhileLoop {
+    public static void main(String[] args) {
+        int i=1;
+        do {
+            System.out.println(i); //is there doing only printing Statement
+            i++; // doing operation part
+        }while (i<=10); // condition
+
+    }
+}
