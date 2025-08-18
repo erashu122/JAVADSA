a7b2595bab90339ab67f6ab21d21310d32bdbc77
erashu122 <kbitu8676@gmail.com>
2025-08-18 20:45:05 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/String/equal.java
@@ -0,0 +1,11 @@
+package DAS_WITH_JAVAADD.String;
+
+public class equal {
+    public static void main(String[] args) {
+        String s="abcde";
+        String b="abc";
+         b+="de";
+        System.out.println(s==b);
+        System.out.println(s.equals(b));
+    }
+}
