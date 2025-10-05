500be883b627704df9ad0b99136817436bdc14a0
erashu122 <kbitu8676@gmail.com>
2025-10-05 11:20:46 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/OOPS_IN_JAVA/StudentClass.java
@@ -0,0 +1,18 @@
+package DAS_WITH_JAVAADD.OOPS_IN_JAVA;
+
+public class StudentClass {
+    // creating a new data type
+    public static class Student{
+        String name;
+        int rollno;
+        double percent;
+    }
+    public static void main(String[] args) {
+        Student x=new Student();
+        x.name="ashutosh";
+        x.rollno=34;
+        x.percent=78.3;
+        System.out.println(x.percent + 10);
+
+    }
+}
