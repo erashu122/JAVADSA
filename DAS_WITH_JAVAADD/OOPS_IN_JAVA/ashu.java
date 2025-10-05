31153acbd1390ac211d09384b79033ef66c4d034
erashu122 <kbitu8676@gmail.com>
2025-10-05 10:55:51 +0530

    first commit

---
+++ DAS_WITH_JAVAADD/OOPS_IN_JAVA/ashu.java
@@ -0,0 +1,20 @@
+package DAS_WITH_JAVAADD.OOPS_IN_JAVA;
+
+class a {
+    public static void main(String[] args) {
+        System.out.println("a class main");
+    }
+}
+class b {
+    public static void main(String[] args) {
+        System.out.println("b class main");
+    }
+}
+class c{
+    public static void main(String[] args) {
+        System.out.println("c class main");
+    }
+}
+class d{
+
+}
